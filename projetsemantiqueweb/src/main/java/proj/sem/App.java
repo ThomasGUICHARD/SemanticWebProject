ed6ecba7e0e8ddcbf412cdc75d8ae93e6257c720
ThomasGUICHARD <thomas.guichard07@gmail.com>
2021-12-03 14:04:39 +0100

    First commit with Jena examples

---
+++ projetsemantiqueweb/src/main/java/proj/sem/App.java
@@ -0,0 +1,20 @@
+package proj.sem;
+
+import java.io.IOException;
+
+import com.opencsv.exceptions.CsvValidationException;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException, CsvValidationException
+    {
+        RDFConstructor rdfConstr= new RDFConstructor();
+
+       /* RDFConstructor rdfConstr2= new RDFConstructor(0);*/
+    }
+}
