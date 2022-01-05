ae3586a8a88b1d342c0cf21e00940d93f7a4fae7
ThomasGUICHARD <thomas.guichard07@gmail.com>
2022-01-05 21:30:41 +0100

    fist step of web interface

--- semweb/src/main/java/com/sw/semweb/controllers/UserController.java
+++ semweb/src/main/java/com/sw/semweb/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.sw.semweb.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.opencsv.exceptions.CsvValidationException;
@@ -16,9 +17,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import net.minidev.json.JSONObject;
 
-
-
-
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionFactory;
 
 @Controller
 public class UserController {
@@ -26,21 +34,34 @@ public class UserController {
    
 
     @RequestMapping(value = { "/" }, method = RequestMethod.GET)
-    public String accueil() throws CsvValidationException, IOException {
-         //RDFConstructor rdfConstr= new RDFConstructor();
+    public String accueil(org.springframework.ui.Model model) throws CsvValidationException, IOException {
+         
+        String datasetURL = "http://localhost:3030/Data";
+        String sparqlEndpoint = datasetURL + "/sparql";
+        String sparqlQuery = datasetURL + "/query";
+        String graphStore = datasetURL + "/data";
+        ArrayList<String> listeRoom =new ArrayList<String>();
+        RDFConnection conneg = RDFConnectionFactory.connect(sparqlEndpoint,sparqlQuery,graphStore);
 
-       /* RDFConstructor rdfConstr2= new RDFConstructor(0);*/
+      
 
-       /*TtlFile f=new TtlFile();
-       RDFSender rdfs=new RDFSender(f);*/
-
-      // RDFConstructor rdfc=new RDFConstructor();
-
-       RDFSender rdfs=new RDFSender();
-       rdfs.RDFGetter();
-       
+        QueryExecution qExec = conneg.query("prefix bot: <https://w3id.org/bot#> SELECT DISTINCT ?s { ?s a bot:Space . }") ;
+        ResultSet rs = qExec.execSelect() ;
+        while(rs.hasNext()) {
+            QuerySolution qs = rs.next() ;
+            Resource subject = qs.getResource("s") ;
+            listeRoom.add(subject.toString());
+            //System.out.println("Subject: "+subject) ;
+        }
+        model.addAttribute("listRoom",listeRoom);
+        
+        qExec.close();
         return "accueil";
     }
-   
+    @RequestMapping(value = "/GetRoomList", method = RequestMethod.GET)
+    public String getRoomList() {
+        
+        return "";
+    }
    
 }
